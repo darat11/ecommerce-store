7063d6f85c3d23d069122a7cbd89b6bb9d4aef53
Meldin Geljić <abdmel1001@gmail.com>
2020-09-27 10:46:01 +0200

    component js files

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ import Contact from "./pages/ContactPage";
 import SingleProduct from "./pages/SingleProductPage";
 import Default from "./pages/DefaultPage";
 
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
+import SideCart from "./components/SideCart";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       {/* navbar, sidebar, cart, footer */}
+      <Navbar />
+      <Sidebar />
+      <SideCart />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
@@ -22,6 +30,7 @@ function App() {
         <Route path="/Product/:id" exact component={SingleProduct} />
         <Route path="/Default" component={Default} />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/SideCart.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SideCart() {
+  return (
+    <div>
+      <h1>SideCart</h1>
+    </div>
+  );
+}
+
+export default SideCart;
